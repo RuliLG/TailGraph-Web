2d770c2d2611cd56e33cf7fe4845de4b3e4ed096
Raúl López <hi@raullg.com>
2021-02-11 20:39:34 +0000

    Refactor

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,16 @@
 <template>
-  <div class="bg-gradient-to-b from-blue-600 to-blue-500 py-16">
-    <div class="container grid grid-cols-2 px-4 sm:px-6 lg:px-8">
-      <div>
-        
-      </div>
-      <div>
-        <TwitterCard title="Generate OG images using TailwindCSS with a free API" description="Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et" />
-      </div>
-    </div>
+  <div>
+    <Banner />
   </div>
 </template>
 
 <script>
-import TwitterCard from '../components/TwitterCard'
+import Banner from '../components/Banner'
 
 export default {
   name: 'index',
   components: {
-    TwitterCard
+    Banner
   }
 }
 </script>
