7785c3ef2cc5b238586dcdecbf5685d719bae8c5
Raúl López <hi@raullg.com>
2021-02-12 19:47:49 +0000

    Fix sticky positioning

--- components/TryOut.vue
+++ components/TryOut.vue
@@ -1,5 +1,5 @@
 <template>
-    <section class="py-12 bg-gradient-to-r from-teal-600 to-blue-500 overflow-hidden lg:py-24" id="try">
+    <section class="py-12 bg-gradient-to-r from-teal-600 to-blue-500 lg:py-24" id="try">
         <div class="container mx-auto px-4 sm:px-6 lg:px-8">
             <h2 class="text-2xl font-bold text-white md:text-4xl" data-aos="fade-down">Try it out</h2>
             <p class="text-lg text-teal-100 mt-4 md:text-xl" data-aos="fade-up">Use our visual editor to create your Open Graph template or, if you feel confident with TailwindCSS, just <a href="#docs" class="underline text-white">go to the docs</a>.</p>
@@ -7,8 +7,10 @@
                 <div>
                     <UrlGenerator :config="config" @update="update" />
                 </div>
-                <div v-if="url">
-                    <TwitterCard class="sticky top-0" :title="config.title" :description="config.text" :href="url" :imageUrl="url" :new-tab="true" />
+                <div v-if="url" class="-mt-8">
+                    <div class="sticky top-0 pt-8">
+                        <TwitterCard :title="config.title" :description="config.text" :href="url" :imageUrl="url" :new-tab="true" />
+                    </div>
                 </div>
             </div>
         </div>
