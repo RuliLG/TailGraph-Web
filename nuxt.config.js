f8b88cd68980cf24ac71371b7bf2086e60fa1c38
Raúl López <hi@raullg.com>
2021-02-13 13:01:28 +0000

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,14 +4,14 @@ export default {
    ** Headers of the page
    */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'TailGraph | Generate your OG Images using TailwindCSS',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
         hid: 'description',
         name: 'description',
-        content: process.env.npm_package_description || '',
+        content: 'Free API to generate open graph images for social network using the power of TailwindCSS.',
       },
     ],
     link: [
@@ -26,7 +26,7 @@ export default {
   /*
    ** Customize the progress-bar color
    */
-  loading: { color: '#fff' },
+  loading: { color: '#3685E2' },
   /*
    ** Global CSS
    */
