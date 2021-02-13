c3a56fa8463b41cbd91c392c43a23938448974d1
Raúl López <hi@raullg.com>
2021-02-13 13:14:05 +0000

    Update Footer.vue

--- components/Footer.vue
+++ components/Footer.vue
@@ -18,7 +18,9 @@
             </div>
             <div class="mt-8 md:mt-0 md:order-1">
                 <p class="text-center text-base text-gray-400">
-                    &copy; 2020 Raúl López. All rights reserved.
+                    Made with 
+                    <svg class="w-6 h-6 inline-block text-red-500 mx-2" fill="currentColor" stroke="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg>
+                    by <a href="https://raullg.com/" target="_blank" rel="noopener noreferrer">Raúl López</a>.
                 </p>
             </div>
         </div>
